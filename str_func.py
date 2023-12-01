b11ba189f1159dbcfe7a7278f3ba4318eb7f9edb
NikaKoipish <152218836+NikaKoipish@users.noreply.github.com>
2023-12-01 22:42:37 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,6 @@
 def func_upper():
     """делает заглавными все буквы строки"""
-    return(input().upper())
+    return(input().upper().strip())
 
 def func_title():
     """делает заглавными первые буквы каждого слова в строке"""
